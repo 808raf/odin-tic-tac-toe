1c8ec7dee32c074487f2f01e723e3d7382e67391
808raf <102700868+808raf@users.noreply.github.com>
2024-03-30 14:49:26 -1000

    Add Gameboard and GameController
    Gameboard is responsible for storing and changing the current board.
    GameController is responsible for the players and game state.

---
+++ script.js
@@ -0,0 +1,93 @@
+function Gameboard() {
+  const board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    console.log(getBoard());
+  };
+
+  const placeMarker = (playerMarker, cell) => {
+    if (board[cell] !== "") {
+      return;
+    }
+
+    board[cell] = playerMarker;
+  };
+
+  return { getBoard, printBoard, placeMarker };
+}
+
+(function GameController() {
+  const board = Gameboard();
+
+  let playerOneName = "Player 1";
+  let playerTwoName = "Player 2";
+
+  const players = [
+    { name: playerOneName, marker: "x" },
+    { name: playerTwoName, marker: "o" },
+  ];
+
+  let activePlayer = players[0];
+  let winner = "";
+
+  const getActivePlayer = () => activePlayer;
+  const setActivePlayer = (playerNum) => {
+    activePlayer = players[playerNum];
+  };
+
+  const switchPlayerTurn = () => {
+    getActivePlayer().name == playerOneName
+      ? setActivePlayer(1)
+      : setActivePlayer(0);
+  };
+
+  const printNewRound = () => {
+    console.log(`It is ${activePlayer.name}'s turn!`);
+  };
+
+  const alertNewRound = () => {
+    return `It is ${activePlayer.name}'s turn!`;
+  };
+
+  const checkWinner = (board) => {
+    const winningMoves = [
+      [0, 1, 2], // Vertical
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6], // Horizontal
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8], // Diagonal
+      [6, 4, 2],
+    ];
+
+    for (let [a, b, c] of winningMoves) {
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        console.log(`${activePlayer.name} is the winner!`);
+        winner = board[a];
+        return board[a];
+      }
+    }
+  };
+
+  const playRound = () => {
+    const cell = prompt(alertNewRound());
+    board.placeMarker(activePlayer.marker, cell);
+    board.printBoard();
+
+    if (winner === "") {
+      checkWinner(board.getBoard());
+      switchPlayerTurn();
+      playRound();
+      console.log(getActivePlayer());
+    }
+  };
+
+  printNewRound();
+  board.printBoard();
+  playRound();
+
+  return { getActivePlayer, printNewRound };
+})();
